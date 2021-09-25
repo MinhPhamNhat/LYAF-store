5325aafa2dbcb4b5be22cd3cd56fcddc943e7a66
PhamNhatMinh <73160254+MinhPhamNhat@users.noreply.github.com>
2021-09-26 00:43:58 +0700

    Update dashboard navbar

--- src/public/js/productManager.js
+++ src/public/js/productManager.js
@@ -4,22 +4,25 @@ const LYAF_header = document.querySelector('.LYAF-header');
 LYAF_header.classList.add('none');
 //
 $(document).ready(()=>{
+    checkActiveElement($(".LYAF-option-active"))
+    showMenu($(".LYAF-option-active").parent().parent())
+
     $(".LYAF-side-bar-header i").on("click", () => {
         if ($(".LYAF-side-bar-header i").hasClass("fa-bars")){
-
             $(".LYAF-side-bar").css("left","0")
-            $(".LYAF-side-bar-menu .LYAF-menu .icon").css("left","0")
+            $(".LYAF-side-bar-menu .LYAF-menu .LYAF-menu-header .icon").css("left","0")
 
-            $(".LYAF-side-bar-menu .LYAF-menu .end").fadeIn(100)
-            $(".LYAF-side-bar-menu .LYAF-menu .title").fadeIn(100)
+            $(".LYAF-side-bar-menu .LYAF-menu .end").fadeIn(500)
+            $(".LYAF-side-bar-menu .LYAF-menu .title").fadeIn(500)
 
             $(".LYAF-side-bar-header i").addClass("fas fa-window-close")
             $(".LYAF-side-bar-header i").removeClass("fas fa-bars")
             $(".LYAF-side-bar-header i").addClass("fas fa-window-close")
 
+            showMenu($(".LYAF-option-active").parent().parent())
         }else if ($(".LYAF-side-bar-header i").hasClass("fa-window-close")){
-            $(".LYAF-side-bar").css("left","-190px")
-            $(".LYAF-side-bar-menu .LYAF-menu .icon").css("left","190px")
+            $(".LYAF-side-bar").css("left","-180px")
+            $(".LYAF-side-bar-menu .LYAF-menu .LYAF-menu-header .icon").css("left","180px")
 
             $(".LYAF-side-bar-menu .LYAF-menu .end").fadeOut(100)
             $(".LYAF-side-bar-menu .LYAF-menu .title").fadeOut(100)
@@ -27,14 +30,80 @@ $(document).ready(()=>{
             $(".LYAF-side-bar-header i").removeClass("fas fa-window-close")
             $(".LYAF-side-bar-header i").addClass("fas fa-bars")
 
+            hideMenu($(".LYAF-menu-active"))
+            for (i of $(".LYAF-menu-active")){
+                if (!$(i).find(".LYAF-menu-content .LYAF-option-active").hasClass("LYAF-option-active")){
+                    $(i).removeClass("LYAF-menu-active")
+                    deactiveMenu(i)
+                }
+            }
         }
     })
 })
 
-const changeIcon = (element) => {
+const checkActiveElement = (element) => {
+    var parentElement = $(element).parent().parent()
+    activeMenu(parentElement)
+    parentElement.addClass("LYAF-menu-active")
+    activeOption(element)
+}
+const activeOption = (element) => {
     $(element).find(".icon").css("color","rgb(72, 153, 190)")
+    $(element).css("background-color","#1B1B28")
+    $(element).find("span").css("color","white")
+    
 }
 
-const changeBackIcon = (element) => {
-    $(element).find(".icon").css("color","rgb(183, 183, 185)")
+const deactiveOption = (element) => {
+    if (!$(element).hasClass("LYAF-option-active")){
+        $(element).find(".icon").css("color","rgb(183, 183, 185)")
+        $(element).css("background-color","#1E1E2D")
+        $(element).find("span").css("color","rgb(183, 183, 185)")
+    }
+}
+
+const activeMenu = (element) => {
+    $(element).find(".LYAF-menu-header .icon").css("color","rgb(72, 153, 190)")
+    $(element).find(".LYAF-menu-header").css("background-color","#1B1B28")
+    $(element).find(".LYAF-menu-header").css("color","white")
+}
+
+const deactiveMenu = (element) => {
+    if(!$(element).hasClass("LYAF-menu-active") && !$(element).find(".LYAF-menu-option").hasClass("LYAF-option-active")){
+        $(element).find(".icon").css("color","rgb(183, 183, 185)")
+        $(element).find(".LYAF-menu-header").css("background-color","#1E1E2D")
+        $(element).find(".LYAF-menu-header").css("color","rgb(183, 183, 185)")
+    }
+}
+
+const showMenu = (element) => {
+    $(element).find(".LYAF-menu-header .end").css("transform","rotate(90deg)")
+    $(element).find(".LYAF-menu-content").slideDown()
+    activeMenu(element)
+
+}
+
+const hideMenu = (element) => {
+    $(element).find(".LYAF-menu-header .end").css("transform","rotate(0deg)")
+    $(element).find(".LYAF-menu-content").slideUp()
+    deactiveMenu(element)
+}
+
+const tongleMenu = (element) => {
+    element = $(element).parent().hasClass("LYAF-menu") ? $(element).parent() : null
+    if(element){
+        if (!$(element).find(".LYAF-menu-content").is(':visible')){
+            for (i of $(".LYAF-menu-active")){
+                if (!$(i).find(".LYAF-menu-content .LYAF-option-active").hasClass("LYAF-option-active")){
+                    $(i).removeClass("LYAF-menu-active")
+                }
+                hideMenu(i)
+            }
+            $(element).addClass("LYAF-menu-active")
+            showMenu(element)
+        }else{
+            $(element).removeClass("LYAF-menu-active")
+            hideMenu(element)
+        }
+    }
 }
