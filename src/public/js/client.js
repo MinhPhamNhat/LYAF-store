c883c20ef168f7946d526dada7c09f5a7f492003
AnNguyen2000 <annguyenkhanh211220@gmail.com>
2021-09-27 16:28:30 +0700

    client

--- src/public/js/client.js
+++ src/public/js/client.js
@@ -15,3 +15,27 @@ var swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+ //Responsive Carousel:
+// const slide = document.querySelectorAll('.img-carousel');
+
+// const carousel1_name = [
+//   'summer-sale-small.jpg',
+//   'new-collection-small.jpg',
+//   'summer-collection-small.jpg'
+ 
+// ];
+// k_slides = slide.length;
+
+//  function ResCarousel1(x) {
+//   if (x.matches) { // If media query matches
+//     for( let i =0; i < k_slides;++i){
+//       slide[i].src = `../img/${carousel1_name[i]}`;
+//     }
+//   }
+  
+// }
+
+// var x = window.matchMedia("(max-width: 900px)");
+// ResCarousel1(x); // Call listener function at run time
+// x.addListener(ResCarousel1); 
