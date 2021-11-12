685d389871d702e6c49a17357853dcf09f3a069a
AnNguyen2000 <annguyenkhanh211220@gmail.com>
2021-11-12 21:58:49 +0700

    aaa

--- src/public/js/client.js
+++ src/public/js/client.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 $(document).ready(()=>{
     var splide = new Splide( '.splide', {
         type   : 'fade',
@@ -17,3 +18,74 @@ $(document).ready(()=>{
         $(`#tab-${dataToggle}`).removeClass('d-none');
     })
 })
+=======
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+
+//Grid Item:
+const grid__item = document.querySelectorAll('.grid__item');
+
+for(let i of grid__item){
+  i.style.height =`calc(${window.getComputedStyle(i).getPropertyValue('width')} + 80px)`;
+  window.addEventListener('resize', (e) =>{
+    i.style.height = `calc(${window.getComputedStyle(i).getPropertyValue('width')} + 80px)`;
+  });
+}
+
+//sale-Product:
+var swiper = new Swiper(".sale_product--swiper", {
+  slidesPerView: 2,
+  spaceBetween: 20,
+  slidesPerGroup: 2,
+  loop: true,
+  loopFillGroupWithBlank: false,
+  breakpoints: {
+    // when window width is <= 499px
+    700: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    900: {
+      slidesPerView: 4,
+      spaceBetween: 15,
+    },
+    1100: {
+      slidesPerView: 5,
+      spaceBetween: 10,
+    },
+    1200: {
+      slidesPerView: 5,
+      spaceBetween: 20,
+    },
+  },
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true,
+  // },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+
+
+
+>>>>>>> Stashed changes
