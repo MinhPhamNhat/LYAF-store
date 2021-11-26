b26ddf09b046bf44bf0453ffa8f5661f29d0ead8
MinhPhamNhat <73160254+MinhPhamNhat@users.noreply.github.com>
2021-11-26 19:50:44 +0700

    fix userinfo

--- src/app/controllers/UserInfoController.js
+++ src/app/controllers/UserInfoController.js
@@ -2,18 +2,25 @@ userModel = require('../models/User');
 class userInfoController{
 
     start(req,res,next){
-        const user = req.user;
-        res.render('userInformation',{user});
+        userModel.findById(req.user._id).lean().exec()
+        .then(user=> {
+            console.log(user)
+            if (user)
+            res.render('userInformation',{user});
+            else
+            res.render('404');
+        }).catch(err=>{
+            res.render('404');
+        })
     }
     userInfoUpdate(req,res,next){
-        userModel.updateOne({_iq:req.user._id},{
+        userModel.updateOne({_iq: req.user._id},{
             name: req.body.profilename,
             phoneNumber: req.body.profilephone,
             email:req.body.profilemail,
-
         }).exec()
-        .then(()=>{
-            res.render('userInformation',{user: req.user});
+        .then((data)=>{
+            res.redirect('/userInfo');
         })
     }
     proStatus(req,res,next){
